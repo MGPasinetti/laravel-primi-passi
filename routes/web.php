c1d6cd9201b54d0ff33e8edc3c5fe719c1c70e41
MGPasinetti <mg.pasinetti@gmail.com>
2022-04-26 16:50:22 +0200

    homepage created

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Route::get('/about', function () {
+//     return '<h1>About our web site</h1>';
+// })-> name('about');
+
+
+/*
+A questo punto, iniziamo a prendere confidenza con le rotte e le views: cancelliamo la view welcome.blade.php e creiamo una nostra homepage. Facciamo quindi sì che la rotta / visualizzi home.blade.php
+Inizialmente stampiamo un Hello World, poi passiamo dei dati alla view in modo da visualizzarli dinamicamente con Blade.
+Bonus:
+Creiamo più di una pagina e visualizziamo un header menu con i link di tutte le pagine, utilizzando la funzione route()
+*/
+
+
+
+
+
+
+
+
+
